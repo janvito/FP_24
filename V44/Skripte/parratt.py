6b656f1713446766e4691c1bcfe06f0926ecf375
janvito <jan@oppoli.de>
2024-05-17 05:15:14 +0200

    auswertung angefangen

--- V44/Skripte/parratt.py
+++ V44/Skripte/parratt.py
@@ -13,8 +13,8 @@ plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 20
 plt.rcParams['lines.linewidth'] = 2
 plt.xlabel(r"$\theta$ $[\si{\degree}]$")
-plt.ylabel(r"Counts")
-
+plt.ylabel(r"$X_1=\frac{R_1}{T_1}$")
+plt.yscale("log")
 def parratt(a_i, del2, del3, sig1, sig2, b2, b3, d2):
     a_irad = np.deg2rad(a_i)
     k = 2 * np.pi / lamda
@@ -40,47 +40,49 @@ def parratt(a_i, del2, del3, sig1, sig2, b2, b3, d2):
 
 # Load data
 theta, counts = np.genfromtxt('../Daten/CSV/probenscan_clean.csv', delimiter=',', unpack=True)
-theta, counts = theta[15:100], counts[15:100]
+theta, counts = theta[15:80], counts[15:80]
 _theta=np.linspace(theta[0],theta[-1],1000)
 counts = np.log(counts)
 layer_thickness = 1.7e-9
 layer_thickness2 = 6.819180819180819e-08
 lamda = 1.54 * 10**(-10)
 initial_guess1 = np.array([7.648577896993004e-06 ,0.0012410968451660746 ,1.8963141350166085e-09 ,3.4233238181111846e-08 ,3.643402232484923e-07*1.5*1.3 ,1.0573950235770515e-05*0.9 ,2.416951357029224e-06])
-initial_guess1 = np.array([7.9557e-06 ,1.2046e-03 ,1.7548e-09 ,3.3993e-08 ,6.2143e-07 ,9.1987e-06 ,2.4208e-06])
-fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+final_guess1 = np.array([7.638322588136867e-06 ,1.608172310206349e-03 ,1.758162616922126e-09 ,3.209885006802162e-08 ,3.991960588052604e-07 ,1.288915703020240e-05 ,2.422157089116472e-06])
+#fig, axes = plt.subplots(2, 3, figsize=(15, 10))
 
-for i, ax in enumerate(axes.flatten()):
-    lowest = 4
-    for k in range(200):
-        # Gaussian perturbation
-        perturbed_guess = np.copy(initial_guess1)
-        for j, guess in enumerate(initial_guess1):
-            perturbed_guess[j] = np.random.normal(guess, abs(guess) * 0.01)
-        try:
-            params, _ = curve_fit(parratt, theta, counts, p0=perturbed_guess, maxfev=50000)
-        except:
-            print("lol")
-        errors = counts - parratt(theta, *params)
-        valid_indices = np.logical_and(np.isfinite(errors), ~np.isnan(errors))
-        squared_errors = errors[valid_indices] ** 2
-        mse = np.sum(squared_errors)
-        
-        if mse < lowest and  mse!=0:
-            lowest = mse
-            print(f"{i}: ", end="")
-            for par in perturbed_guess:
-                print(f"{par:.4e}", ",", end="")
-            print("\tms: ", mse)
-            # Plot the curve on the current subplot
-            ax.plot(theta, counts, label="data")
-            ax.plot(theta, parratt(theta, *params), label="fit")
-            ax.set_title(f"Iteration {i+1}")
-            ax.set_xlabel("Theta")
-            ax.set_ylabel("Intensity")
-            ax.grid(True)
-            break  # Exit the while loop when MSE condition is met
-
+#for i, ax in enumerate(axes.flatten()):
+#    lowest = 2
+#    for k in range(200):
+#        # Gaussian perturbation
+#        perturbed_guess = np.copy(initial_guess1)
+#        for j, guess in enumerate(initial_guess1):
+#            perturbed_guess[j] = np.abs(np.random.normal(guess, abs(guess) * 0.01))
+#        try:
+#            params, _ = curve_fit(parratt, theta, counts, p0=perturbed_guess, maxfev=50000)
+#        except:
+#            print("lol")
+#        errors = counts - parratt(theta, *params)
+#        valid_indices = np.logical_and(np.isfinite(errors), ~np.isnan(errors))
+#        squared_errors = errors[valid_indices] ** 2
+#        mse = np.sum(squared_errors)
+#        
+#        if mse < lowest and  mse!=0:
+#            #initial_guess1=params
+#            lowest = mse
+#            print(f"{i}: ", end="")
+#            for par in params:
+#                print(f"{par:.15e}", ",", end="")
+#            print("\tms: ", mse)
+#            # Plot the curve on the current subplot
+#            ax.plot(theta, counts, label="data")
+#            ax.plot(theta, parratt(theta, *params), label="fit")
+#            ax.set_title(f"Iteration {i+1}")
+#            ax.set_xlabel("Theta")
+#            ax.set_ylabel("Intensity")
+#            ax.grid(True)
+#            break  # Exit the while loop when MSE condition is met
+plt.plot(theta,np.exp(counts),label=r"Gemessene Reflexivität")
+plt.plot(theta,np.exp(parratt(theta,*final_guess1)),label=r"Parratt-Reflexivität")
 plt.tight_layout()
 plt.show()
 # Add legend outside the loop
--- V44/Skripte/probe.py
+++ V44/Skripte/probe.py
@@ -5,7 +5,7 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 from scipy.signal import find_peaks
 import scipy.constants as c
-
+from uncertainties import unumpy as unp
 # Enable LaTeX rendering in Matplotlib
 plt.rc('text', usetex=True)
 plt.rc('text.latex', preamble=r'\usepackage{siunitx}')
@@ -14,23 +14,27 @@ plt.rcParams['figure.figsize'] = (10, 8)
 plt.rcParams['font.size'] = 20
 plt.rcParams['lines.linewidth'] = 2
 
+colors = ['gray', 'gold','darkorange', 'lightcoral', 'saddlebrown',  'lightpink', 'pink']
+
 # Constants
 D = 0.02 # Aus dem X-Scan in m
+Du= ufloat(D,0.001)
 d0 = 0.00016 # Aus dem Z-Scan in m
+d0u= ufloat(d0,0.00002)
 Z = 14 
 rho = 2.336
 r_0 = 5.29e-11
 Mm = 28 * c.u
 N = rho * Z / Mm
 lamda = 1.5*10**(-10)
-print("Nr0: ",N*r_0)
-Nr0=20*10**10
-delta_ = N * r_0 * lamda ** 2 / (2 * np.pi) #Formel aus Quellen
-print("delta aus Werten mit N selber berechnet: ",delta_)
-delta_ = Nr0 * lamda ** 2 / (2 * np.pi) #Formel aus Quellen
-print("delta aus Werten aus Skript: ",delta_)
-delta = 7.6 * 10 ** (-6) #literaturwert
-print("delta aus Anleitungs: ",delta)
+print("Nr0: ", N*r_0)
+Nr0 = 20*10**10
+delta_ = N * r_0 * lamda ** 2 / (2 * np.pi) # Formel aus Quellen
+print("delta aus Werten mit N selber berechnet: ", delta_)
+delta_ = Nr0 * lamda ** 2 / (2 * np.pi) # Formel aus Quellen
+print("delta aus Werten aus Skript: ", delta_)
+delta = 7.6 * 10 ** (-6) # Literaturwert
+print("delta aus Anleitungs: ", delta)
 delta_polysterol = 3.5*10**(-6)
 Imax = 445506 * 5 # Aus dem Detektorscan
 
@@ -39,15 +43,14 @@ def G(alpha):
     return (D * np.sin(alpha * 2 * np.pi / 360) / d0)
 
 def alphat(alpha):
-    return np.sqrt(alpha**2-a_c_silicium**2)
+    return np.sqrt(alpha**2 - a_c_silicium**2)
 
 def fresnel2(thetas):
-    retheta=[]
+    retheta = []
     for theta in thetas:
         if theta < a_c_silicium:
             retheta.append(1)
         else:
-            #theta-=thetaclean[43]
             retheta.append(((theta - alphat(theta)) / (theta + alphat(theta)))**2)
     return retheta
 
@@ -59,7 +62,7 @@ plt.yscale('log')
 # Load and process data
 a_c_silicium = np.sqrt(2 * delta) * 360 / (2 * np.pi)
 a_c_polysterol = np.sqrt(2 * delta_polysterol) * 360 / (2 * np.pi)
-print("a_c=",a_c_silicium)
+print("a_c=", a_c_silicium)
 theta1, counts1 = np.genfromtxt('../Daten/CSV_Output/MessungProbe.csv', delimiter=',', skip_header=1, unpack=True)
 theta2, counts2 = np.genfromtxt('../Daten/CSV_Output/MessungProbeDiffus.csv', delimiter=',', skip_header=1, unpack=True)
 theta1, counts1, theta2, counts2 = theta1[theta1 < 1], counts1[theta1 < 1], theta2[theta1 < 1], counts2[theta1 < 1]
@@ -71,21 +74,22 @@ countsclean = counts1 - counts2
 
 # Plot ideal Fresnel reflectivity
 fresnelcount = fresnel2(thetaclean)
-plt.plot(thetaclean, fresnelcount, "gray", alpha=0.7, label=r'Ideale Fresnelreflektivität für Silizium')
+plt.plot(thetaclean, fresnelcount, color=colors[0], alpha=0.7, label=r'Ideale Fresnelreflektivität für Silizium')
 
 # Plot raw data
-plt.plot(theta1, counts1, "b-", label=r'Reflektivitätsscan')
-plt.plot(theta2, counts2, "b-", alpha=0.5, label=r'Diffuser Scan')
+plt.plot(theta1, counts1, color=colors[1], label=r'Reflektivitätsscan')
+plt.plot(theta2, counts2, color=colors[2], label=r'Diffuser Scan')
 
 # Apply corrections
 a_g = np.arcsin(d0 / D) * 360 / (2 * np.pi)
-print("a_g=",a_g)
-a_g_gemessen=0.3820697736481243
+a_gu = unp.arcsin(d0u / Du) * 360 / (2 * np.pi)
+print("a_g=", f"{a_gu:.3f}")
+a_g_gemessen = 0.3820697736481243
 countsG = countsclean.copy()
 
 for i in range(np.shape(thetaclean)[0]):
     if thetaclean[i] < a_g:
-        countsG[i] = countsG[i]/G(thetaclean[i])
+        countsG[i] = countsG[i] / G(thetaclean[i])
 
 # Find peaks and calculate layer thickness
 peaks, props = find_peaks(countsG)
@@ -97,13 +101,13 @@ T_u = ufloat(T_a, T_s)
 layer_thickness = lamda / (2 * T_u)
 print("Schichtdicke= ", layer_thickness)
 
-
 # Plot corrected data
-plt.plot(thetaclean, countsclean, "k-", label=r'Bereinigter Scan')
-plt.plot(thetaclean, countsG, "r-", label=r'Korrigierter Scan')
+plt.plot(thetaclean, countsclean, color=colors[3], label=r'Bereinigter Scan')
+plt.plot(thetaclean, countsG, colors[4], label=r'Korrigierter Scan')
 plt.plot(thetaclean[peaks], countsG[peaks], "rx")
-plt.axvline(a_c_silicium,color='green',label=r'Kritischer Winkel Silizium')
-plt.axvline(a_c_polysterol,color='green',alpha=0.5,label=r'Kritischer Winkel Polysterol')
+plt.axvline(a_c_silicium, color=colors[5], label=r'Kritischer Winkel Silizium')
+plt.axvline(a_c_polysterol, color=colors[6], alpha=0.5, label=r'Kritischer Winkel Polysterol')
+
 # Save the plot
 plt.legend(prop={'size': 16})
 plt.tight_layout()
